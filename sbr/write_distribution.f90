2c4ef591c58ef7e7c3f894d9762115ee21d50084
Alex <temper8@gmail.com>
2022-11-17 02:22:49 +0300

    Update write_distribution.f90

--- sbr/write_distribution.f90
+++ sbr/write_distribution.f90
@@ -1,25 +1,25 @@
  
-        subroutine write_distribution(fj,N,time)
+subroutine write_distribution(fj,N,time)
+    implicit none
 
-        implicit none
-        integer i, N, iunit
-        integer itime
-        parameter (iunit = 20)
-        real*8 time
-        real*8 fj(*)
-        character(80) fname
-        print *, " fortran 90"
-        itime = INT(time*100000)
-        if (MOD(itime, 10) == 0) then
-            print *, N, time, itime, MOD(itime, 10)
-            write(fname,'("lhcd/distribution/dstr", f9.7,".dat")') time
-            !print *, fname
-            open(iunit,file=fname,position="append")
-            do i=1,n
-     	        write(iunit,*) i, fj(i)
-            end do
-            close(iunit)
-        end if
-        end subroutine
+    integer i, N
+    integer itime
+    integer, parameter :: iunit = 20
+    real*8 time
+    real*8 fj(*)
+    character(80) fname
+
+    itime = INT(time*100000)
+    if (MOD(itime, 10) == 0) then
+        !print *, N, time, itime, MOD(itime, 10)
+        write(fname,'("lhcd/distribution/dstr", f9.7,".dat")') time
+        !print *, fname
+        open(iunit,file=fname,position="append")
+        do i=1,n
+            write(iunit,*) i, fj(i)
+        end do
+        close(iunit)
+    end if
+end subroutine
 
 
