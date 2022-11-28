6f6a03bade90e01683fcfaad7e932e7687c16eab
Alex <temper8@gmail.com>
2022-11-28 19:50:46 +0300

    Create fokkerplank1D.f90

---
+++ sbr/fokkerplank1D.f90
@@ -0,0 +1,8 @@
+
+subroutine fokkerplanck1D(vj, fj, dtstep)
+    implicit none
+    integer, parameter :: i0=1002
+    real*8,dimension(:),allocatable:: y,x,xx,xxm,xxp,a,b,c,f
+    real*8,dimension(:),allocatable:: vj,fj,dfj,d1,d2,d3,givi
+    
+end subroutine fokkerplanck1D
