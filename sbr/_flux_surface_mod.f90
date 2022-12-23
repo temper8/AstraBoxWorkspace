49139735e466c7e928d3e3d8ba3c3e144f030c42
Alex <temper8@gmail.com>
2022-12-23 04:29:33 +0300

    Update _flux_surface_mod.f90

--- sbr/_flux_surface_mod.f90
+++ sbr/_flux_surface_mod.f90
@@ -1,8 +1,23 @@
-module FluxSurface_mod ! the module name defines the namespace
-    type FluxSurface ! classname is the class prototype name
-        integer        :: direction
-        real*8         :: enorm
-
+module FluxSurface_mod
+    !+ все что связанно с магнитными поверхностями
+    type FluxSurface 
+    !+ класс магнитной поверхности
+        integer       :: index
+        !+ номер магнитной поверхности
+        real (real64) :: r
+        !+ радиус
+        real (real64) :: vmax
+        !+ vmax=cltn/vto
+        real (real64) :: vt
+        !+ наверно тепловая скорость электронов????? vt=fvt(r)
+        integer       :: ipt
+        !+ размер vgrid
+        real (real64) :: vgrid(:)
+        !+ 
+        real (real64) :: vr_grid(:)
+        !+ бываший vrj
+        real (real64) :: diffusion(:)
+        !+ бывший dijk(i,j,k) или dj(i)
         !   complex         :: inst_field1
         contains
         procedure :: set   => set_e
