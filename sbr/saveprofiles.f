6b3bcfa83f90efa02e7c777bf95fe09021ccea48
Alex <temper8@gmail.com>
2023-02-21 20:50:54 +0300

    Update saveprofiles.f

--- sbr/saveprofiles.f
+++ sbr/saveprofiles.f
@@ -11,24 +11,7 @@
       include 'for/parameter.inc'
       include 'for/const.inc'
       include 'for/status.inc'
-      integer im,ip
-      real*8 p_in
-       ! real*8 anz,apz,share
-      !real*8 pchm0
-      !real*8 fpol,dfmy
-      !real*8 xlogj
-      !real*8 znak_tor,znak_pol
-      !common/left/ znak_tor,znak_pol
-      !real*8 ynzmp(1001),pmp(1001),ynzmm(1001),pmm(1001)
-
-      !real*8 efld(100),r,vmax
-      !real*8 zff,fnr,fnrr
-      !real*8 pn,gst,dens,tmp,vt,vclt
-      !real*8 znak
       integer :: calls = 0
-      !save share
-
-      p_in=dble(QLH)    ! input LH power, MW
 
       if(calls.eq.0) then
             call read_parameters('lhcd/ray_tracing.dat')
@@ -38,12 +21,12 @@
      & AMETR,RHO,SHIF,ELON,TRIA,MU,NE,TE,TI,ZEF,UPL)
 
       call calc_enorm
-      
- !     open(96, file='lhcd/out/difsave.dat', position='append')
+     
       if(calls.eq.0) then
             call init_maxwell
             calls=1
+            pause
       end if
-!      close(96)
+
       end
 
